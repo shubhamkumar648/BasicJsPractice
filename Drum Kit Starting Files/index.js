cf349cf15433b643c44a756ef3a033d4de17185b
shubham <ciscoshubham123@gmail.com>
2021-07-12 18:22:18 +0530

    add drum file

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,22 @@
+
+
+// const drumEl = document.querySelector('button')
+  
+// drumEl.addEventListener("click",handleclick)
+
+// function handleclick() {
+
+//     alert (' i am click')
+// }
+
+
+var NumberofDrum = document.querySelectorAll('drum').length
+
+for (let i =0; i <=NumberofDrum; i++) {
+
+    addEventListener("click", function () {alert (" i am click")})
+
+}
+
+
+// document.querySelector('button').addEventListener('click', function() { alert ("i am click")})
